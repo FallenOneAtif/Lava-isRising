2339ce28df45fa04210996007c05e5e927cf8a21
Crimson-Hearts <156532977+FallenOneAtif@users.noreply.github.com>
2025-12-15 23:40:43 +0500

    Bit of Improvements

--- Lava is Rising/Assets/Scripts/Grappler.cs
+++ Lava is Rising/Assets/Scripts/Grappler.cs
@@ -1,4 +1,5 @@
 using Unity.VisualScripting;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,6 +11,8 @@ public class Grappler : MonoBehaviour
     public Rigidbody2D Rb;
     public DistanceJoint2D lineDistance;
     public Camera MainCam;
+    public Transform GunPoint;
+    Vector2 DirectionToMove;
     private void Awake()
     {
         GrappleAction = inputActions.actionMaps[0].actions[0];
@@ -28,10 +31,16 @@ public class Grappler : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        Vector2 Mouse1 = Mouse.current.position.ReadValue();
+        Vector2 MousePos = MainCam.ScreenToWorldPoint(Mouse1);
+        Vector2 Direction = MousePos - Rb.position;
+
+        float rot = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg - 90f;
+        Rb.rotation = rot;
+
         if (lineDistance.enabled)
         {
-            GrappleLine.SetPosition(1, transform.position);
+            GrappleLine.SetPosition(1, GunPoint.position);
         }
     }
     public void GrappleActivate()
@@ -44,13 +53,12 @@ public class Grappler : MonoBehaviour
         }
         if (RayData.collider.CompareTag("GrapplePoint"))
         {
-
             lineDistance.connectedAnchor = (Vector2)RayData.transform.position;
             GrappleLine.SetPosition(0, (Vector2)RayData.transform.position);
-            GrappleLine.SetPosition(1, transform.position);
+            GrappleLine.SetPosition(1, GunPoint.position);
             GrappleLine.enabled = true;
             lineDistance.enabled = true;
-            Debug.Log(Rb.angularVelocity);
+            Debug.Log(Rb.linearVelocity);
         }
         
     }
@@ -61,6 +69,6 @@ public class Grappler : MonoBehaviour
     }
     public void OvertimeForce(Transform ForcePoint)
     {
-        
+
     }
 }
